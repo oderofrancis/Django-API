2409baa85eba74a1ae41b765a0b2fda1e3f497ef
oderofrancis <francisodero10@gmail.com>
2023-04-14 10:13:27 +0300

    Phase one done

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','subtitle','author','isbn')
+
+admin.site.register(Book,BookAdmin)
--- web/settings.py
+++ web/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
