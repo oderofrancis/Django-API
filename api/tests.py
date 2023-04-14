89b5469392e01cb05b6e57bc05096d7672594116
oderofrancis <francisodero10@gmail.com>
2023-04-14 10:22:02 +0300

    Test for models and project done

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import *
 
 # Create your tests here.
+
+class BookTests(TestCase):
+    @classmethod
+
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = "Harry Potter",
+            subtitle = "The Philosopher's Stone",
+            author = "J.K. Rowling",
+            isbn = "9780747532743"
+        )
+    
+    def test_book_content(self):
+        self.assertEqual(f"{self.book.title}", "Harry Potter")
+        self.assertEqual(f"{self.book.subtitle}", "The Philosopher's Stone")
+        self.assertEqual(f"{self.book.author}", "J.K. Rowling")
+        self.assertEqual(f"{self.book.isbn}", "9780747532743")
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Harry Potter')
+        self.assertTemplateUsed(response, 'api/book_list.html')
